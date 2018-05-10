2f15180e9ea1e3c2bbe7705dd47c71c76b1e3f25
M Haidar Hanif <git@mhaidarhanif.com>
2018-05-11 04:40:21 +0700

    Update powered by logos

--- src/components/PoweredBy.js
+++ src/components/PoweredBy.js
@@ -1,28 +1,53 @@
-import React from 'react';
-import {Component} from 'react';
-import '../theme/poweredby.css';
-import ReactJsLogo from '../assets/poweredby/reactjs.png';
-import MongooseLogo from '../assets/poweredby/mongoose.jpg';
-import BootstrapLogo from '../assets/poweredby/bootstrap.png';
-import JsLogo from '../assets/poweredby/js.png';
+import React from "react";
+import { Component } from "react";
 
-class PoweredBy extends Component{
-  render(){
-    return(
+import "../theme/poweredby.css";
+
+import ReactJsLogo from "../assets/poweredby/reactjs.png";
+import MongooseLogo from "../assets/poweredby/mongoose.jpg";
+import MongooDBLogo from "../assets/poweredby/mongodb.png";
+import BootstrapLogo from "../assets/poweredby/bootstrap.png";
+import JsLogo from "../assets/poweredby/js.png";
+
+class PoweredBy extends Component {
+  render() {
+    return (
       <div>
-        <br/>
-        <h1 className="poweredbylabel">PoweredBy</h1>
+        <br />
+        <h1 className="poweredbylabel">Powered by</h1>
         <div className="poweredbyflex">
           <div className="fixsizepoweredby">
-            <a href="#" ><img className="poweredbyiconsize poweredbycentral" src={ReactJsLogo} /> </a>
+            <a href="#">
+              <img
+                className="poweredbyiconsize poweredbycentral"
+                src={ReactJsLogo}
+              />
+            </a>
           </div>
 
           <div className="fixsizepoweredby">
-            <a href="#" ><img className="poweredbyiconsize poweredbycentral" src={MongooseLogo} /> </a>
+            <a href="#">
+              <img
+                className="poweredbyiconsize poweredbycentral"
+                src={MongoDBLogo}
+              />
+            </a>
           </div>
 
           <div className="fixsizepoweredby">
-            <img className="poweredbyiconsize poweredbycentral" src={BootstrapLogo} />
+            <a href="#">
+              <img
+                className="poweredbyiconsize poweredbycentral"
+                src={MongooseLogo}
+              />
+            </a>
+          </div>
+
+          <div className="fixsizepoweredby">
+            <img
+              className="poweredbyiconsize poweredbycentral"
+              src={BootstrapLogo}
+            />
           </div>
 
           <div className="fixsizepoweredby">
@@ -32,11 +57,9 @@ class PoweredBy extends Component{
           <div className="fixsizepoweredby">
             <img className="poweredbyiconsize poweredbycentral" src={JsLogo} />
           </div>
-
         </div>
       </div>
-
-    )
+    );
   }
 }
 export default PoweredBy;
